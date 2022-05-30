089017209c5e537b5959b100274660b17900fffe
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-30 17:09:33 +0200

    Regisetr mongo Context

---
+++ FindX.WebApi/Extenstions/Services.cs
@@ -0,0 +1,12 @@
+﻿using FindX.WebApi.Services;
+
+namespace FindX.WebApi.Extenstions
+{
+	public static class Services
+	{
+		public static void RegisterServices(this WebApplicationBuilder builder)
+		{
+			builder.Services.AddSingleton<IMongoContext, MongoContext>();
+		}
+	}
+}
