09423a21e00aa26671e259a794824a283ed555a2
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-07-02 14:36:27 +0200

    Modify search

--- FindX.WebApi/Repositories/Repository/SearchRepository.cs
+++ FindX.WebApi/Repositories/Repository/SearchRepository.cs
@@ -53,27 +53,36 @@ namespace FindX.WebApi.Repositories.Repository
 
 		public async Task<IEnumerable<Item>> FullSearchFinderAsync(string query)
 		{
-			var pipeLine = new BsonDocument[]
-				{
-					new BsonDocument("$search",
-					new BsonDocument
+			var searchPipe = new BsonDocument[]
+			{
+			new BsonDocument("$search",
+					new BsonDocument {
+					{
+						"index",
+						"searchItems"
+					},
+					{
+						"text",
+						new BsonDocument {
 							{
-									{ "index", "BebarsSearch" },
-									{ "autocomplete",
-					new BsonDocument
-									{
-											{ "path", "Title" },
-											{ "query", ""+query+"" },
-											{ "fuzzy",
-					new BsonDocument("maxEdits", 1) }
-									} }
-							}),
-					new BsonDocument("$limit", 10),
-				};
-			var searchResult = await _context.Items
-				.Aggregate<Item>(pipeLine)
+								"query",
+								"{\"title\": {$eq: " + query + "}}"
+							},
+							{
+								"path",
+								new BsonDocument("wildcard", "*")
+							},
+							{
+								"fuzzy",
+								new BsonDocument()
+							}
+						}
+					}
+			})};
+
+			return await _context.Items
+				.Aggregate<Item>(searchPipe)
 				.ToListAsync();
-			return searchResult;
 		}
 	}
 }
