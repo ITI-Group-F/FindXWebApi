174b53c9a311612dac91321a8e65e1a6895c2bab
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-07-03 13:48:41 +0200

    Modify register dto

--- FindX.WebApi/Controllers/AuthenticationController.cs
+++ FindX.WebApi/Controllers/AuthenticationController.cs
@@ -3,69 +3,67 @@ using Microsoft.AspNetCore.Mvc;
 using FindX.WebApi.DTOs;
 using FindX.WebApi.Services;
 
-namespace FindX.WebApi.Controllers
+namespace FindX.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
 {
+	private readonly IAuthenticateService _Authenticate;
 
-	[Route("api/[controller]")]
-	[ApiController]
-	public class AuthenticationController : ControllerBase
+	public AuthenticationController(IAuthenticateService Authenticate)
 	{
-		private readonly IAuthenticateService _Authenticate;
-
-		public AuthenticationController(IAuthenticateService Authenticate)
-		{
-			_Authenticate = Authenticate;
-		}
-
-		[HttpPost]
-		[Route("login")]
-		public async Task<IActionResult> Login(LoginDto model)
-		{
-			var result = await _Authenticate.Login(model);
-			if (result != null)
-			{
-				return Ok(result);
-			}
-			return BadRequest("Wrong user name or password");
-		}
-
-		[HttpPost]
-		[Route("register-user")]
-		public async Task<IActionResult> Register(RegisterDto model)
-		{
-			var result = await _Authenticate.Register(model);
-			if (result != null)
-			{
-				return Ok(result);
-			}
-			return BadRequest("User maybe exists or incorrect password format");
-		}
-
-		[Authorize(Roles = "Admin")]
-		[HttpPost]
-		[Route("register-admin")]
-		public async Task<IActionResult> RegisterAdmin(RegisterDto model)
-		{
-			var result = await _Authenticate.RegisterAdmin(model);
-			if (result != null)
-			{
-				return Ok(result);
-			}
-			return BadRequest("User maybe exists or incorrect password format");
-		}
-
-		[Authorize(Roles = "Admin")]
-		[HttpPost]
-		[Route("create-role")]
-		public async Task<IActionResult> CreateRole(string name)
-		{
-			var result = await _Authenticate.CreateRole(name);
-			if (result != null)
-			{
-				return Ok(result);
-			}
-			return BadRequest("Role already exists");
-		}
-
+		_Authenticate = Authenticate;
 	}
+
+	[HttpPost]
+	[Route("login")]
+	public async Task<IActionResult> Login(LoginDto model)
+	{
+		var result = await _Authenticate.Login(model);
+		if (result != null)
+		{
+			return Ok(result);
+		}
+		return BadRequest("Wrong user name or password");
+	}
+
+	[HttpPost]
+	[Route("register-user")]
+	public async Task<IActionResult> Register(RegisterDto model)
+	{
+		var result = await _Authenticate.Register(model);
+		if (result != null)
+		{
+			return Ok(result);
+		}
+		return BadRequest("User maybe exists or incorrect password format");
+	}
+
+	[Authorize(Roles = "Admin")]
+	[HttpPost]
+	[Route("register-admin")]
+	public async Task<IActionResult> RegisterAdmin(RegisterDto model)
+	{
+		var result = await _Authenticate.RegisterAdmin(model);
+		if (result != null)
+		{
+			return Ok(result);
+		}
+		return BadRequest("User maybe exists or incorrect password format");
+	}
+
+	[Authorize(Roles = "Admin")]
+	[HttpPost]
+	[Route("create-role")]
+	public async Task<IActionResult> CreateRole(string name)
+	{
+		var result = await _Authenticate.CreateRole(name);
+		if (result != null)
+		{
+			return Ok(result);
+		}
+		return BadRequest("Role already exists");
+	}
+
 }
--- FindX.WebApi/DTOs/Authenticate/RegisterDto.cs
+++ FindX.WebApi/DTOs/Authenticate/RegisterDto.cs
@@ -5,6 +5,15 @@ namespace FindX.WebApi.DTOs;
 public class RegisterDto
 {
 	[Required]
+	public string FirstName { get; set; }
+
+	[Required]
+	public string LastName { get; set; }
+
+	[Required]
+	public string Phone { get; set; }
+
+	[Required]
 	public string Username { get; set; }
 
 	[EmailAddress]
